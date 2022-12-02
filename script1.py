f3293fad32909b60b0a07e74e676ead2e2a1cf82
Zahar Falyhov <zahar.falyhov@list.ru>
2022-12-02 17:59:21 +0300

    script1

--- script1.py
+++ script1.py
@@ -2,11 +2,17 @@ x, y = 0, 0
 print(f"Начальная точка: ({x}, {y})")
 route = input("Введите направление движения: ")
 distance = float(input("Введите количество шагов: "))
-if route.lower() == "вниз" or route.lower() == "вверх":
+if route.lower() == "вниз":
+    y -= distance
+    print(f"Текущее положение: ({x}, {y})")
+elif route.lower() == "вверх":
     y += distance
     print(f"Текущее положение: ({x}, {y})")
-elif route.lower() == "вправо" or route.lower() == "влево":
+elif route.lower() == "вправо":
     x += distance
+    print(f"Текущее положение: ({x}, {y})")
+elif route.lower() == "влево":
+    x -= distance
     print(f"Текущее положение: ({x}, {y})")
 else:
     print("Не знаю как туда попасть)))))")
